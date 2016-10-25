7681de54c86054cab44399ec6bf51dea663a63f8
Chrysovalantis Anastasiou <chrysovalantis.anastasiou@gmail.com>
2016-10-24 18:55:20 -0700

    Improved ReviewsSpider structure

--- yelp-extractor/YelpReviewsSpider.py
+++ yelp-extractor/YelpReviewsSpider.py
@@ -2,6 +2,7 @@ import scrapy
 
 
 class ReviewItem(scrapy.Item):
+    restaurant_id = scrapy.Field()
     review_id = scrapy.Field()
     user_id = scrapy.Field()
     user_name = scrapy.Field()
@@ -30,35 +31,36 @@ class YelpReviewsSpider(scrapy.Spider):
             self.start_urls.append(self.base_url + b_id)
 
     def parse(self, response):
-        for review_obj in response.css('div.review-list ul.reviews li div.review:not(.js-war-widget)'):
-            yield from self.parse_review(review_obj)
+        yield from self.parse_reviews(response)
 
         next_link = response.css('a.next::attr("href")').extract_first()
         if next_link is not None:
             yield scrapy.Request(next_link, callback=self.parse)
 
-    def parse_review(self, response):
-        review = ReviewItem()
+    def parse_reviews(self, response):
+        restaurant_id = response.url.split("/")[-1].split("?")[0]
+        for review_obj in response.css('div.review-list ul.reviews li div.review:not(.js-war-widget)'):
+            review = ReviewItem()
 
-        review['review_id'] = response.css('div.review::attr("data-review-id")').extract_first()
-        raw_text = response.css('div.review-content p::text').extract()
-        review['review_text'] = "".join(raw_text)
-        review['review_date'] = response.css('span.rating-qualifier::text').extract_first().strip()
-        raw_rating = response.css('div.biz-rating i::attr("title")').extract_first().split(" ")
-        review['review_rating'] = float(raw_rating[0])
+            review['restaurant_id'] = restaurant_id
+            review['review_id'] = review_obj.css('div.review::attr("data-review-id")').extract_first()
+            raw_text = review_obj.css('div.review-content p::text').extract()
+            review['review_text'] = "".join(raw_text)
+            review['review_date'] = review_obj.css('span.rating-qualifier::text').extract_first().strip()
+            raw_rating = review_obj.css('div.biz-rating i::attr("title")').extract_first().split(" ")
+            review['review_rating'] = float(raw_rating[0])
 
-        raw_user = response.css('div.review-sidebar li.user-name a')
-        raw_user_id = raw_user.css('::attr("href")').extract_first().split("=")
-        review['user_id'] = raw_user_id[1]
-        review['user_name'] = raw_user.css('::text').extract_first()
+            raw_user = review_obj.css('div.review-sidebar li.user-name a')
+            raw_user_id = raw_user.css('::attr("href")').extract_first().split("=")
+            review['user_id'] = raw_user_id[1]
+            review['user_name'] = raw_user.css('::text').extract_first()
 
-        review['review_votes'] = self.parse_votes(response.css('ul.voting-buttons li'))
-        yield review
+            review['review_votes'] = self.parse_votes(review_obj.css('ul.voting-buttons li'))
+            yield review
 
     def parse_votes(self, response):
         votes = ReviewVotes()
 
-        # TODO parse votes
         for button in response:
             vote_type = button.css('span.vote-type::text').extract_first().lower()
             vote_count = button.css('span.count::text').extract_first()
